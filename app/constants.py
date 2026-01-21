43b3de88a2bf490eb1c0d239bef4dbd3589e2d16
Parthib Mukherjee <parthibmukherjee@gmail.com>
2026-01-21 18:53:40 +0530

    url change Signed-off-by: Parthib Mukherjee <parthibmukherjee@gmail.com>

--- app/constants.py
+++ app/constants.py
@@ -3,8 +3,8 @@
 # Audio URLs for different interaction types
 GREETING_AUDIO_MAP = {
     "entry": "https://firebasestorage.googleapis.com/v0/b/bwi-cabswalle.appspot.com/o/Raahi%2Fgreeting.wav?alt=media&token=93970efd-eaf5-45bd-850a-ed9cdac4856b",
-    "duty_audio": "https://firebasestorage.googleapis.com/v0/b/bwi-cabswalle.appspot.com/o/Raahi%2Fgreeting.wav?alt=media&token=93970efd-eaf5-45bd-850a-ed9cdac4856b",
-    "cng_pumps": "https://firebasestorage.googleapis.com/v0/b/bwi-cabswalle.appspot.com/o/Raahi%2Fgreeting.wav?alt=media&token=93970efd-eaf5-45bd-850a-ed9cdac4856b",
+    "duty_audio": "https://firebasestorage.googleapis.com/v0/b/bwi-cabswalle.appspot.com/o/Raahi%2Fduty.wav?alt=media&token=7a74cbad-8e82-42a5-9c86-b19ee619ef67",
+    "cng_pumps": "https://firebasestorage.googleapis.com/v0/b/bwi-cabswalle.appspot.com/o/Raahi%2Fcng.wav?alt=media&token=7b213349-af22-4d3b-84ba-26cd814926ef",
     "petrol_pumps": "https://firebasestorage.googleapis.com/v0/b/bwi-cabswalle.appspot.com/o/Raahi%2Fgreeting.wav?alt=media&token=93970efd-eaf5-45bd-850a-ed9cdac4856b",
     # Future greetings can be added here:
     # "morning_greeting": "https://...",
